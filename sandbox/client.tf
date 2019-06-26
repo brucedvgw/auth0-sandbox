760e77650e2568e76e23b7081b0a81a058ef66d3
brucedvgw <bruce.dominguez@vgw.co>
2019-06-26 12:16:41 +0800

    update provider

--- sandbox/client.tf
+++ sandbox/client.tf
@@ -9,11 +9,5 @@ resource "auth0_client" "my_app_client" {
   web_origins     = ["https://example.com"]
   grant_types     = ["authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "refresh_token"] #https://auth0.com/docs/applications/reference/grant-types-available
 
-  jwt_configuration = {
-    lifetime_in_seconds = 360000
-    secret_encoded      = false
-    alg                 = "RS256"
-  }
-
   custom_login_page_on = "true"
 }
